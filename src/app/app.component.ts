7f267654de531ddc33a69f815e8a377c28c27d26
unknown <webberic92@yahoo.com>
2018-09-26 21:38:31 -0400

    Added some CSS.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
 })
+
 export class AppComponent {
-  title: string = 'Angular Google Maps API Example 2018';
+  title: string = 'Google Maps Example 2018';
   lat: number = 29.207033;
   lng: number = -81.057931;
 }
